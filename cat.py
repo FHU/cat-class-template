1f3e3827d2bfa115256abb87df4aba8b027c222a
Gabe-Matherley <157724600+Gabe-Matherley@users.noreply.github.com>
2024-02-16 11:27:27 -0600

    update
    first try

--- cat.py
+++ cat.py
@@ -1,6 +1,20 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__(self, name='unknown', age=0):
+        self.name = name
+        self.age = age
+    def speak(self):
+        return "Meow"
+
+
+stella.name = Stella 
+stella.age = 7
+
+garfield.name = Garfield  
+garfield.age = 50
+
+print(garfield.speak())
+print(stella.speak())
 
 
 #Create objects here
